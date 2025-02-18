f037570136965bb45117cfe279b39788bf07f057
Hayato-Isagawa <harnet0119@yahoo.co.jp>
2025-02-18 14:52:27 +0900

    add:hdrの画像を追加、記述を追加

--- src/components/ThreeCanvas.jsx
+++ src/components/ThreeCanvas.jsx
@@ -2,6 +2,7 @@
 import { useEffect } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
 
 function ThreeCanvas() {
   useEffect(() => {
@@ -11,7 +12,18 @@ function ThreeCanvas() {
     document.body.appendChild(canvas);
 
     const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.VSMShadowMap;
+
     const scene = new THREE.Scene();
+
+    const loader = new RGBELoader();
+    loader.load("/src/assets/three/three01.hdr", (texture) => {
+      texture.mapping = THREE.EquirectangularReflectionMapping;
+      scene.background = texture;
+      scene.environment = texture;
+    });
+
     const camera = new THREE.PerspectiveCamera(
       32,
       window.innerWidth / window.innerHeight,
@@ -22,21 +34,58 @@ function ThreeCanvas() {
 
     const controls = new OrbitControls(camera, canvas);
     const geometry = new THREE.SphereGeometry();
-    const material = new THREE.MeshStandardMaterial();
+    const material = new THREE.MeshStandardMaterial({
+      color: new THREE.Color("white"),
+      roughness: 0.1,
+      metalness: 0.5,
+      // emissive: new THREE.Color("rgb(20, 20, 20)"),
+    });
     const mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 
+    // 直接光源
     const directionalLight = new THREE.DirectionalLight(
       new THREE.Color("white"),
       0.75
     );
     directionalLight.position.set(2, 2, 2);
+    directionalLight.castShadow = true;
+    directionalLight.shadow.blurSamples = 30;
+    directionalLight.shadow.radius = 12;
     scene.add(directionalLight);
-    scene.add(new THREE.DirectionalLightHelper(directionalLight));
+
+    const dirHelper = new THREE.DirectionalLightHelper(directionalLight);
+    // scene.add(dirHelper);
+
+    // 点光源
+    const pointLight = new THREE.PointLight(0xffffff, 0.1);
+    pointLight.position.set(2, 0, 2);
+    // scene.add(pointLight);
+
+    const pointHelper = new THREE.PointLightHelper(pointLight);
+    // scene.add(pointHelper);
+
+    // 環境光
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+    // scene.add(ambientLight);
 
     const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x000000, 0.25);
-    scene.add(hemisphereLight);
-    scene.add(new THREE.HemisphereLightHelper(hemisphereLight));
+    // scene.add(hemisphereLight);
+
+    const hemiHelper = new THREE.HemisphereLightHelper(hemisphereLight);
+    scene.add(hemiHelper);
+
+    const planeGeo = new THREE.PlaneGeometry(10, 10);
+    const plane = new THREE.Mesh(planeGeo, material);
+    plane.position.set(0, -1, 0);
+    plane.rotation.set(Math.PI * -0.5, 0, 0);
+    scene.add(plane);
+
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+
+    plane.receiveShadow = true;
+    plane.castShadow = true;
 
     const render = () => {
       requestAnimationFrame(render);
