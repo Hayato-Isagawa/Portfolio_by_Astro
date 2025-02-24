f990a8a52bcdda233bdd149cded72733dfd3e267
Hayato-Isagawa <harnet0119@yahoo.co.jp>
2025-02-24 20:06:52 +0900

    add:modelにアニメーションを追加

--- src/components/three/ThreeCanvas01.jsx
+++ src/components/three/ThreeCanvas01.jsx
@@ -4,7 +4,7 @@ import { THREE, OrbitControls, generateCanvas, GLTFLoader } from "./Setting.js";
 const ThreeCanvas01 = () => {
   useEffect(() => {
     const canvas = generateCanvas();
-    let renderer, scene, camera, controls;
+    let renderer, scene, camera, controls, gltfModel;
 
     renderer = new THREE.WebGLRenderer({
       canvas,
@@ -16,7 +16,7 @@ const ThreeCanvas01 = () => {
       32,
       window.innerWidth / window.innerHeight
     );
-    camera.position.set(0, 0, 10);
+    camera.position.set(5, 5, 5);
     controls = new OrbitControls(camera, canvas);
 
     const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
@@ -24,6 +24,9 @@ const ThreeCanvas01 = () => {
     scene.add(directionalLight);
 
     directionalLight.castShadow = true;
+    directionalLight.shadow.mapSize.width = 2048;
+    directionalLight.shadow.mapSize.height = 2048;
+    directionalLight.shadow.radius = 8;
 
     const sphere = new THREE.SphereGeometry();
     const material = new THREE.MeshStandardMaterial();
@@ -33,23 +36,45 @@ const ThreeCanvas01 = () => {
     const loader = new GLTFLoader();
     loader.load("/src/assets/three/Catalina_Clay_Product.glb", (gltf) => {
       const model = gltf.scene;
+      console.log(gltf);
       model.traverse((obj) => {
         if (obj.isMesh) {
-          console.log(obj);
           obj.castShadow = true;
           obj.receiveShadow = true;
+          obj.material.metalness = 0;
         }
       });
+      gltfModel = model;
       scene.add(model);
+
+      // pattern2 ファイル自体にアニメーション処理を加えている場合
+      // mixer = new THREE.AnimationMixer(model);
+      // gltf.animations.forEach((clip) => {
+      //   mixer.clipAction(clip).play();
+      // });
+
+      render();
     });
+
+    const animate = () => {
+      // pattern1 ファイル自体にアニメーション処理を加えていない場合
+      gltfModel.rotation.x += 0.01;
+      gltfModel.rotation.y += 0.01;
+      gltfModel.rotation.z += 0.01;
+
+      // pattern2 ファイル自体にアニメーション処理を加えている場合
+      // if (mixer) {
+      //   mixer.update(1 / 60);
+      // }
+    };
 
     const render = () => {
       requestAnimationFrame(render);
       renderer.render(scene, camera);
       controls.update();
-    };
 
-    render();
+      animate();
+    };
 
     return () => {
       document.body.removeChild(canvas);
